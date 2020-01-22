07aac7470cf369119e548ba540f4879c2dbd48df
Adrian <adrianivsdk@gmail.com>
2020-01-22 12:08:11 -0300

    Initial setup

---
+++ src/Components/Home.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import '../Styles/Home.scss'
+
+const Home = props => {
+  return (
+    <div className="Home">
+      <h1>MOVE</h1>
+      <h3>Quem somos</h3>
+    </div>
+  );
+};
+
+export default Home;
